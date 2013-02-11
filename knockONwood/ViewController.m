20e3dd690df2637f457ebfb36021eb875260d6a3
Jennifer Clark <jac300@nyu.edu>
2013-02-11 16:41:02 -0500

    Initial Commit

---
+++ knockONwood/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  knockONwood
+//
+//  Created by Jennifer Clark on 2/11/13.
+//  Copyright (c) 2013 Jennifer Clark. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
